ccddc118e09e344edc60e0c28c3d887a4f732dca
kmamaguana <kmamaguana@uce.edu.ec>
2024-11-11 14:09:58 -0500

    feat: :memo:

--- obj/Debug/net8.0/PrimeraPaginaC.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeraPaginaC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeraPaginaC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d25b7c06b9543a8935ff60c40d5283fc3abaa537")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeraPaginaC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeraPaginaC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
